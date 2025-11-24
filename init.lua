4ea2b1ad14e261135f750abdc3338fdd452fedbb
Gynt <gynt@users.noreply.github.com>
2025-11-24 22:53:58 +0100

    fix: extreme wasn't detected properly

--- init.lua
+++ init.lua
@@ -4,12 +4,19 @@ if #arg > 0 then
     log(VERBOSE, string.format("writing steamapp_id.txt file"))
     local f, err = io.open("steam_appid.txt", 'w')
     if not f then error(err) end
-    if arg[1] == "Stronghold_Crusader_Extreme.exe" then
+    local haystack = (arg[1]):lower()
+    local needle1 = ("Stronghold Crusader.exe"):lower()
+    local needle2 = ("Stronghold_Crusader_Extreme.exe"):lower()
+    local s1, e1 = haystack:find(needle1)
+    local s2, e2 = haystack:find(needle2)
+    if s2 ~= nil and e2 ~= nil and e2 == needle2:len() then
         log(VERBOSE, string.format("writing steamapp_id.txt file for Extreme: 16700"))
         f:write("16700")
-    else
+    elseif s1 ~= nil and e1 ~= nil and e1 == needle1:len() then
         log(VERBOSE, string.format("writing steamapp_id.txt file for Crusader: 40970"))
         f:write("40970")
+    else
+        log(VERBOSE, string.format("leaving steamapp_id.txt untouched: could not identify game variant"))
     end
     f:close()
 end
